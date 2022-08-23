d7886f964984e9e7278251d660f3e78294d90700
Nikita <smidt2017kg@gmail.com>
2022-08-23 20:12:10 +0600

    Calculator-done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import Buttons from './Buttons';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      out: '0'
+    }
+    this.refOutput = React.createRef()
+  }
+
+
+  tapeNumber(value) {
+    let currentValue = value
+    let output = this.refOutput.current
+    this.setState({
+      out: currentValue
+    })
+    if (output.value === '0') {
+      output.value = ''
+    }
+    output.value += currentValue
+  }
+
+  tapeWork(value) {
+    let output = this.refOutput.current
+
+    if (value === 'AC') {
+      output.value.length === 1 ? output.value = '0' : output.value = output.value.substring(0, output.value.length - 1)
+     
+    }
+    else if (value == 'C') { output.value = '0' }
+    else if (value === '=') {
+    
+      try {output.value = eval(output.value) }
+      catch { output.value = 'Это невозможно' }
+      setTimeout(()=> {
+        output.value = 0
+      }, 1500)
+    }
+  }
+
+  render() {
+    return (
+
+      <div className="container">
+        <h1>Calculator</h1>
+        <div className="output">
+          <input ref={this.refOutput} type="text" defaultValue={this.state.out} />
+        </div>
+        <div className="buttons">
+          {Buttons.buttons.map((item,index) => {
+            return <button key={index} onClick={() => {
+              this.tapeNumber((item.val))
+            }}>{item.val}</button>
+          })}
+
+          {Buttons.work.map((item,index) => {
+            return <button key={index } onClick={() => {
+              this.tapeWork((item.val))
+            }}>{item.val}</button>
+          })}
+        </div>
+      </div>
+    )
+  }
 }
-
 export default App;
